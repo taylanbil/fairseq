67c5a012d1457a9815d1876da2f0926dfea93909
vaibhav singh <sivaibhav@gmail.com>
2020-08-20 22:17:49 +0000

    clean up

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -531,6 +531,9 @@ class Trainer(object):
             try:
                 with maybe_no_sync():
                     # forward and backward
+                    # FIXME: remove
+                    print("DEBUG_MESSAGE: Sample-Size: ", sample['net_input']['source'].size())
+
                     loss, sample_size_i, logging_output = self.task.train_step(
                         sample=sample,
                         model=self.model,
