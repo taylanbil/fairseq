Correct accuracy computation, refactor xla tensor check.

fairseq/criterions/wav2vec_criterion.py
@@ -31,8 +31,7 @@ class Wav2VecCriterionConfig(FairseqDataclass):
         default_factory=lambda: [],
         metadata={"help": "output keys to log"},
     )
-
-from fairseq.utils import index_put
+from fairseq.utils import index_put, is_xla_tensor
 
 @register_criterion("wav2vec", dataclass=Wav2VecCriterionConfig)
 class Wav2vecCriterion(FairseqCriterion):
@@ -54,7 +53,7 @@ class Wav2vecCriterion(FairseqCriterion):
         logits = model.get_logits(net_output).float()
         target = model.get_targets(sample, net_output)
 
-        if logits.device.type == 'xla':
+        if is_xla_tensor(logits):
             # tpu-comment: since dynamic shapes lead to recompilations on xla,
             # we don't shrink tensors using mask_indices.
             # Instead, we do the following when computing loss:
@@ -84,8 +83,7 @@ class Wav2vecCriterion(FairseqCriterion):
 
         if 'sample_size' in sample and self.infonce:
             sample_size = sample['sample_size']
-        elif 'mask_indices' in sample['net_input'] and self.infonce:
-            # XXX: what happens if not self.infonce?
+        elif 'mask_indices' in sample['net_input']:
             sample_size = sample['net_input']['mask_indices'].sum()
         else:
             sample_size = target.numel() if self.infonce else target.long().sum().item()
@@ -117,7 +115,7 @@ class Wav2vecCriterion(FairseqCriterion):
         for lk in self.log_keys:
             if lk in net_output:
                 value = net_output[lk]
-                if not torch.is_tensor(value) or value.device.type != 'xla':
+                if not is_xla_tensor(value):
                     value = float(value)
                 logging_output[lk] = value
 
@@ -134,15 +132,20 @@ class Wav2vecCriterion(FairseqCriterion):
                     assert logits.dim() > 1, logits.shape
                     max = logits.argmax(-1) == 0
                     min = logits.argmin(-1) == 0
-                    both = max & min
-                    # corr = max.long().sum().item() - both.long().sum().item()
-                    corr = max.long().sum() - both.long().sum()
-                    count = float(max.numel())
+                    if is_xla_tensor(logits):
+                        max, min = max * mi, min * mi
+                        both = max & min
+                        corr = max.long().sum() - both.long().sum()
+                        count = mi.sum()
+                    else:
+                        both = max & min
+                        corr = max.long().sum().item() - both.long().sum().item()
+                        count = float(max.numel())
 
                 logging_output["correct"] = corr
                 logging_output["count"] = count
 
-        if log_pred and logits.device.type != 'xla':
+        if log_pred and not is_xla_tensor(logits):
             logging_output['logits'] = logits.cpu().numpy()
             logging_output['target'] = target.cpu().numpy()
         return loss, sample_size, logging_output

fairseq/models/wav2vec/wav2vec2.py
@@ -26,7 +26,7 @@ from fairseq.modules import (
     TransposeLast,
 )
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
-from fairseq.utils import buffered_arange, index_put
+from fairseq.utils import buffered_arange, index_put, is_xla_tensor
 
 
 EXTRACTOR_MODE_CHOICES = ChoiceEnum(["default", "layer_norm"])
@@ -443,8 +443,7 @@ class Wav2Vec2Model(BaseFairseqModel):
 
         logits = logits / self.logit_temp
 
-        if logits.device.type == 'xla' or neg_is_pos.any():
-            #pass
+        if is_xla_tensor(logits) or neg_is_pos.any():
             fillval = -float(2**30)
             if not hasattr(self, '_inftensor'):
                 self._inftensor = torch.tensor(fillval).to(x.device)
@@ -504,7 +503,7 @@ class Wav2Vec2Model(BaseFairseqModel):
                 mask_indices=mask_indices,
                 mask_channel_indices=mask_channel_indices,
             )
-            if x.device.type != 'xla' and mask_indices is not None:
+            if not is_xla_tensor(x) and mask_indices is not None:
                 # tpu-comment: reducing the size in a dynamic way causes
                 # too many recompilations on xla.
                 y = unmasked_features[mask_indices].view(
@@ -558,7 +557,7 @@ class Wav2Vec2Model(BaseFairseqModel):
             else:
                 negs, _ = self.sample_negatives(y, y.size(1))
 
-        if x.device.type != 'xla':
+        if not is_xla_tensor(x):
             # tpu-comment: reducing the size in a dynamic way causes
             # too many recompilations on xla.
             x = x[mask_indices].view(x.size(0), -1, x.size(-1))

fairseq/utils.py
@@ -661,8 +661,12 @@ def tpu_data_loader(itr):
         start=getattr(itr, "n", 0),
         total=len(itr),
     )
+def is_xla_tensor(tensor):
+    return torch.is_tensor(tensor) and tensor.device.type == 'xla'
+
+
 def index_put(tensor, indices, value):
-    if tensor.device.type == 'xla':
+    if is_xla_tensor(tensor):
         for _ in range(indices.dim(), tensor.dim()):
             indices = indices.unsqueeze(-1)
         if indices.size(-1) < tensor.size(-1):