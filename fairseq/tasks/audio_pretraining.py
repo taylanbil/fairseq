d74b45eda9ca32e17e2ebd5a8417e0ebbb17785b
Bilge Acun <acun@fb.com>
2021-01-21 15:55:24 -0800

    fixing configs for precompute mask indices

--- fairseq/tasks/audio_pretraining.py
+++ fairseq/tasks/audio_pretraining.py
@@ -99,26 +99,26 @@ class AudioPretrainingConfig(FairseqDataclass):
             "help": "flag to compute mask indices in data preparation.",
         },
     )
+    if precompute_mask_indices:
+        # The following are needed to precompute mask and mask channel indices
+        #   before model's forward.
+        mask_length: int = II("model.mask_length")
+        mask_prob: float = II("model.mask_prob")
+        mask_selection: str = II("model.mask_selection")
+        mask_other: float = II("model.mask_other")
+        no_mask_overlap: bool = II("model.no_mask_overlap")
+        mask_min_space: int = II("model.mask_min_space")
+        mask_channel_length: int = II("model.mask_channel_length")
+        mask_channel_prob: float = II("model.mask_channel_prob")
+        mask_channel_selection: str = II("model.mask_channel_selection")
+        mask_channel_other: float = II("model.mask_channel_other")
+        no_mask_channel_overlap: bool = II("model.no_mask_channel_overlap")
+        mask_channel_min_space: int = II("model.mask_channel_min_space")
 
-    # The following are needed to precompute mask and mask channel indices
-    #   before model's forward.
-    mask_length: int = II("model.mask_length")
-    mask_prob: float = II("model.mask_prob")
-    mask_selection: str = II("model.mask_selection")
-    mask_other: float = II("model.mask_other")
-    no_mask_overlap: bool = II("model.no_mask_overlap")
-    mask_min_space: int = II("model.mask_min_space")
-    mask_channel_length: int = II("model.mask_channel_length")
-    mask_channel_prob: float = II("model.mask_channel_prob")
-    mask_channel_selection: str = II("model.mask_channel_selection")
-    mask_channel_other: float = II("model.mask_channel_other")
-    no_mask_channel_overlap: bool = II("model.no_mask_channel_overlap")
-    mask_channel_min_space: int = II("model.mask_channel_min_space")
+        conv_feature_layers: str = II("model.conv_feature_layers")
+        encoder_embed_dim: int = II("model.encoder_embed_dim")
 
-    conv_feature_layers: str = II("model.conv_feature_layers")
-    encoder_embed_dim: int = II("model.encoder_embed_dim")
-
-    tpu: bool = II("common.tpu")
+        tpu: bool = II("common.tpu")
 
 
 @register_task("audio_pretraining", dataclass=AudioPretrainingConfig)
@@ -157,23 +157,26 @@ class AudioPretrainingTask(FairseqTask):
         return cls(cfg, target_dictionary=target_dictionary)
 
     def _get_mask_precompute_kwargs(self, cfg):
-        args = [
-            'mask_length',
-            'mask_prob',
-            'mask_selection',
-            'mask_other',
-            'no_mask_overlap',
-            'mask_min_space',
-            'mask_channel_length',
-            'mask_channel_prob',
-            'mask_channel_selection',
-            'mask_channel_other',
-            'no_mask_channel_overlap',
-            'mask_channel_min_space',
-            'encoder_embed_dim',
-            'conv_feature_layers',
-        ]
-        return {arg: cfg[arg] for arg in args}
+        if self.cfg.precompute_mask_indices:
+            args = [
+                'mask_length',
+                'mask_prob',
+                'mask_selection',
+                'mask_other',
+                'no_mask_overlap',
+                'mask_min_space',
+                'mask_channel_length',
+                'mask_channel_prob',
+                'mask_channel_selection',
+                'mask_channel_other',
+                'no_mask_channel_overlap',
+                'mask_channel_min_space',
+                'encoder_embed_dim',
+                'conv_feature_layers',
+            ]
+            return {arg: cfg[arg] for arg in args}
+        else:
+            return {}
 
     def load_dataset(self, split: str, task_cfg: FairseqDataclass = None, **kwargs):
         data_path = self.cfg.data
