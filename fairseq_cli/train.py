d565859bfc3f1191a8431023b89de1327c516fef
Taylan Bilal <taylanbil@gmail.com>
2021-01-29 19:41:52 +0000

    Skip last batch in validation.

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -354,7 +354,14 @@ def validate(
         # create a new root metrics aggregator so validation metrics
         # don't pollute other aggregators (e.g., train meters)
         with metrics.aggregate(new_root=True) as agg:
-            for sample in progress:
+            for i, sample in enumerate(progress):
+                if cfg.common.tpu and i == len(progress)-1:
+                    import torch_xla.core.xla_model as xm
+                    print(
+                        'ORDINAL {}: SKIPPING sample {}, proglen {}'.format(
+                            xm.get_ordinal(), i, len(progress))
+                    )
+                    break
                 trainer.valid_step(sample)
 
         # log validation stats
